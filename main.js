ce20f65a2e981dd4250970a88eea7dd90998c19a
sunnatazizov <122353586+sunnatazizov@users.noreply.github.com>
2023-04-21 07:57:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,148 @@
+//Елка
+
+document.write(`Eid Mubarak <br>`)
+
+for (let i = 1; i <= 5; i++) {
+    for (let k = 5; k >= i; k--) {
+        document.write(` * `)
+    }
+    document.write(`<br>`)
+}
+
+//Калькулятор
+
+// for (let i = 2; i <= 9; i++) {
+//     for (let k = 1; k <= 10; k++) {
+//       document.write(`${i} * ${k} = ${i * k} <br>` );
+//     }
+//   }
+
+
+//Курсы
+
+let arr = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+
+
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+]
+
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {
+        course: '   BACKEND',
+        count: 0
+    },
+]
+
+
+for (let person of arr) {
+    for (let category of categories) {
+        if (person.info.faculity.toLowerCase() === category.course.trim().toLowerCase()) {
+            category.count++;
+            break;
+        }
+    }
+}
+console.log(categories);
